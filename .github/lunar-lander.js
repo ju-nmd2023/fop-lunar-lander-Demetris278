ae4351ba960a7a8fa3d33f02a83ff500ccd72e7a
Demetris278 <150178664+Demetris278@users.noreply.github.com>
2024-02-19 17:40:38 +0200

    updates

--- .github/lunar-lander.js
+++ .github/lunar-lander.js
@@ -11,33 +11,59 @@ let y4=50;
 let gravity=0.1;
 let thrust=0;
 let velocityY=0;
-
-
-
-function keyPressed(){
+let screen=2;
+let s_start_game = "Start Game";
+let starsX=[];
+let starsY=[];
+let starsAlpha=[];
+let starsDrawn = false;
+function checkInput(){
     if(keyIsDown(87)){
-        if(thrust==0){
-            velocityY = 0;
-            thrust= 0.3;
+        console.log(thrust);
+        if(thrust===0){
+            //velocityY = 0;
+            thrust= 0.1;
         }
         else{
-        thrust= thrust * 1.5;
+            thrust= thrust * 1.05;
         }
-        if (thrust > 3){
-            thrust = 3;
+        if (thrust > 0.5){
+            thrust = 0.5;
         }
     }
-    console.log(thrust);
+    else{
+        thrust = 0;
+    }
+    
+    /*console.log(thrust);*/
 }
 
-function draw(){
-    background(0,0,0);
+function obstaclesMovement(){
+    if (starsDrawn === false){
+        for (let i = 0; i < 30; i++){
+            starsX[i] = Math.random() * 775 +  25;
+            starsY[i] = Math.random() * 550 + 50;
+            starsAlpha[i]=Math.random() * 255;
+        }
+        starsDrawn = true;
+    }
+    for (let i = 0; i<30; i++){
+        fill(255,255,255,starsAlpha[i]);
+        x = starsX[i];
+        y = starsY[i];
+        ellipse(x,y,5,5);
+        starsAlpha[i]
+    }
+}
+function characterMovement(){
+    checkInput();
+    fill(100,200,250);
     quad(x1,y1,x2,y2,x3,y3,x4,y4);
     //console.log(velocityY);
     
     velocityY=velocityY + gravity - thrust;
-    if (velocityY < -3){
-        velocityY = -3;
+    if (velocityY < -4){
+        velocityY = -4;
     }
     if(y3<700 && y4<700 ){ 
         y1=y1+velocityY;
@@ -55,13 +81,57 @@ function draw(){
     }
 }
 
+function drawPlatform(){
+    fill(86,86,86);
+    ellipse(400,750,1000,400);
+    noStroke();
+    fill(120,120,120);
+    ellipse(220,740,180,140);
+    ellipse(560,650,140,120);
+    ellipse(620,840,160,140);
+    fill(211,197,44);
+    ellipse(400,700,150,40);
+}
 
+function drawStart(){
+    fill(255,255,255);
+    textSize(100);
+    textAlign(CENTER);
+    text(s_start_game,450,300);
 
+    textSize(60);
+    textAlign(CENTER);
+    fill(255,255,20);
+    text("Click A to start",450,400);
 
+    if (keyIsDown(65)){
+        //console.log(screen);
+        clear();
+        screen=2;
+    }
+}
 
+function drawGame(){
+    obstaclesMovement();
+    drawPlatform();
+    
+    characterMovement();
 
+    
+}
 
-
+function draw(){
+    background(0,0,0);
+    //console.log(screen);
+    if (screen ===1){
+        drawStart();
+    }
+    
+    //Game - Screen 2
+    if (screen===2){
+        drawGame();
+    }
+}
 
 
 
